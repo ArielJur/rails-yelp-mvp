340af8e68f1f3609218eb86944a4a5596e7b4781
Ariel Jurkevitch <arieljurkevitch@gmail.com>
2022-08-17 17:58:13 +0200

    rails YELP

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
+  # get 'reviews/index'
+  # get 'reviews/new'
+  # get 'reviews/create'
+  # get 'restaurants/index'
+  # get 'restaurants/new'
+  # get 'restaurants/show'
+  # get 'restaurants/create'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new'
+  # get 'restaurants/:id', to: 'restaurants#show'
+  # post 'restaurants', to: 'restaurants#create'
+  # get "restaurants/:id/reviews/new", to: 'reviews#new'
+  # post "restaurants/:id/reviews",
+
+  resources :restaurants, only: [:index, :new, :show, :create] do
+    resources :reviews, only: [:new, :create, :show]
+
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+pink_mama = { name: 'Pink Mama', address: '20bis Rue de Douai, 75009 Paris', category: 'italian' }
+malro =  { name: 'Malro', address: '7 Rue Froissard, 75003 Paris', category: 'french' }
+big_love = { name: 'BigLove', address: '30 Rue Debelleyme, 75003 Paris', category: 'italian' }
+gucci_osteria = { name: 'Gucci Osteria', address: 'P.za della Signoria, 10, Firenze FI, Italie', category: 'italian' }
+gigi = { name: 'Gigi', address: '15 Av. Montaigne, 75008 Paris', category: 'italian' }
+
+[pink_mama, malro, big_love, gucci_osteria, gigi].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
